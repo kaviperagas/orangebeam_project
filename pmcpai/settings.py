872d95ab63555d620d173f3e720ca0e48295bf6e
kaviperagas <peragaskavi@gmail.com>
2023-02-13 00:15:57 +0800

    Project Update

--- pmcpai/settings.py
+++ pmcpai/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = "/home/kaviperagas/orangebeam_project/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
