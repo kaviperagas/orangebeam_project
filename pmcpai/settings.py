50112b94c222cb2d5fa38aeb53d8d5cb1d268c2c
kaviperagas <peragaskavi@gmail.com>
2023-02-13 00:19:15 +0800

    Project Update

--- pmcpai/settings.py
+++ pmcpai/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 STATIC_ROOT = "/home/kaviperagas/orangebeam_project/static"
 
